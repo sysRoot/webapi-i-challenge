3b7879dc938d9c80082d17e5a54ee62ecc5e4856
Jason Cook <the.root.user@gmail.com>
2019-07-30 12:24:15 -0400

    Post and basic get working

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const server = express();
 
 const db = require('./data/db.js');
 
-server.use(express.json()); 
+server.use(express.json());
 
-server.post('/api/users', (req, res) => {
-    
+server.post('/users', (req, res) => {
+    const userEntry = req.body;
+    if (userEntry.name === false || userEntry.bio === false) {
+        return res.status(404).json({ errorMessage: "Please provide name AND bio, for user entry" })
+    }
+    db
+        .insert(userEntry).then((user) => {
+            res.status(201).json(user)
+        })
+        .catch((err) => {
+            res.status(500).json({ error: "Processing User Error, perhaps try again? Make sure to complete the form data" });
+        })
 })
 
-server.get('/api/users', (req, res) => {
+server.get('/users', (req, res) => {
+	db
+		.find()
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: 'The users information could not be retrieved.' });
+		});
+})
 
-} )
+server.listen(3333, () => {
+    console.log('Listening on 3333');
+})

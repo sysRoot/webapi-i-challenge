4e40ec2a905e7b072791ae468b5000f450b6edac
Jason Cook <the.root.user@gmail.com>
2019-07-30 09:37:06 -0400

    Start project of web api

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+const db = require('./data/db.js');
+
+server.use(express.json()); 
+
+server.post('/api/users', (req, res) => {
+    
+})
+
+server.get('/api/users', (req, res) => {
+
+} )
